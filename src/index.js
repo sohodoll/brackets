9acda579353f5ab08b9f245acf1b49646afc786c
sohodoll <kovalevvtl@gmail.com>
2022-08-04 17:03:47 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets_pairs = {}
+    bracketsConfig.forEach (element => {
+        brackets_pairs[`${element[1]}`] = `${element[0]}`;
+    })
+    let stack = [];
+    for (let i = 0; i<str.length; i++) {
+        let currentSymbol = str[i];
+        let topElement = stack[stack.length-1];
+        if (stack.length!=0 && topElement === brackets_pairs[currentSymbol]) {
+            stack.pop();
+        } else {
+            stack.push(currentSymbol);
+        }
+    }
+    return stack.length === 0;
 }
